150378f7c26efce720351eaae60b1765ce676354
johnjosh11 <john.boothapati11@gmail.com>
2019-03-27 10:48:39 +0530

    test

--- cookbooks/Test/recipes/default.rb
+++ cookbooks/Test/recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+file '/tmp/test1' do
+    content 'Hello world'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
